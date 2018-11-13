bfe51017cfaf696b2d103134a69e73127002bb45
bogeresiraje <hannzjavas@gmail.com>
2018-11-13 22:05:28 +0300

    change connection name from local host to cinemaxapp.heroku.com

--- cinemax/config.py
+++ cinemax/config.py
@@ -4,6 +4,5 @@ import os
 class Configuration(object):
     DEBUG = True
     APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
-    #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@localhost:5432/cinemax_db'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@cinemaxapp.herokuapp.com/:5432/cinemax_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
